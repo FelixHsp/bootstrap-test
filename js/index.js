4116c7758fa9108d267fb979f5ed9ed950d111d7
Felix <13115412392@163.com>
2018-09-06 17:23:30 +0800

    9/6

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-   var oLearn = document.getElementById('container-learn');
-   var aLi = oLearn.getElementsByTagName('li');
-   var aImg = oLearn.getElementsByTagName('img');
-   var aSpan = oLearn.getElementsByTagName('span');
-   for (var i = 0; i < aLi.length; i++) {
-       aLi[i].index = i;
-       aLi[i].onclick = function () {
-           for (var i = 0; i < aLi.length; i++) {
-               aLi[i].className = '';
-               aImg[i].className = '';
-               aSpan[i].className = 'hidden';
-           }
-           this.className = 'selected';
-           aImg[this.index].className = 'img-selected';
-           aSpan[this.index].className = ''
-       };
-   }
+var oLearn = document.getElementById('container-learn');
+var aLi = oLearn.getElementsByTagName('li');
+var aSpan = oLearn.getElementsByTagName('span');
+var aIcon = oLearn.getElementsByTagName('p');
+for (var i = 0; i < aLi.length; i++) {
+    aLi[i].index = i;
+    aLi[i].onclick = function () {
+        for (var i = 0; i < aLi.length; i++) {
+            aLi[i].className = '';
+            aSpan[i].className = 'hidden';
+            aIcon[i].className=''
+        }
+        this.className = 'selected';
+        aSpan[this.index].className = ''
+        aIcon[this.index].className='icon-selected'
+    };
+}
